e8e45f5d30d6eec2fd50d7980402293547084398
AlexianeBA <barbealexiane07@gmail.com>
2025-02-07 22:37:15 +0100

    create front app && install daisyui

---
+++ frontend/src/components/TheWelcome.vue
@@ -0,0 +1,15 @@
+<template>
+  <h1 class="text-3xl font-bold underline">
+    Hello world!
+  </h1>
+  <img alt="Vue logo" :src="logo" class="logo" />
+  <button class="btn color-base-100">Default</button>
+  <input type="text" placeholder="Type here" class="input" />
+</template>
+
+<script setup>
+import { ref } from 'vue';
+import logo from '@/assets/images/logo.png';
+
+
+</script>
